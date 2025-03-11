ec07d20b609ee97135574c703f7970b9fcdc923c
ggdefe <ggdefe@gmail.com>
2025-03-11 12:19:47 +0800

    [+] 初始化

---
+++ src/main/java/com/github/ghdefe/LocalOverwriteApolloApplication.java
@@ -0,0 +1,13 @@
+package com.github.ghdefe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalOverwriteApolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalOverwriteApolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ghdefe/LocalOverwriteApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ghdefe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalOverwriteApolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
